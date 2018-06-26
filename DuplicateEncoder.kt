c89b2a47449cca837051289ae3f6f305ddf3154e
Leonid Tumoyakov <leonid.tumoyakov@gmail.com>
2018-06-26 16:54:46 +0700

    Add files via upload

---
+++ DuplicateEncoder.kt
@@ -0,0 +1,36 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+    while (reader.hasNextLine()){
+        var str = reader.nextLine()
+        //var strRes = str
+        var arrayInt = mutableMapOf<Char, Int>()
+        str = str.toLowerCase()
+        var i = 0
+        while (i < str.length) {
+            //var mul = false
+            if (!arrayInt.containsKey(str[i])) {
+                var res = 1;
+                var j: Int = i + 1
+                while (j < str.length) {
+                    if (str[i] == str[j]) {
+                        res++
+                    }
+                    j++
+                }
+                arrayInt.put(str[i],res)
+            }
+            i++
+        }
+        for (item in arrayInt) {
+            if(item.value>1)
+                str = str.replace(item.key,')')
+            else
+                str = str.replace(item.key,'(')
+        }
+        println(str)
+    }
+}
