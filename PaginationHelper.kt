9325d2de9781cca5fb0c598bd0d42e9c1257d57b
lipgen <leonid.tumoyakov@gmail.com>
2018-06-29 17:45:31 +0700

    correction

--- PaginationHelper.kt
+++ PaginationHelper.kt
@@ -1,53 +1,25 @@
 
 fun main(args: Array<String>) {
-    val array = Array(15,{i->"string #$i"});
+    val array = Array(15,{"string #$it"});
     val paginationHelper = PaginationHelper(array,10)
     println(paginationHelper.length())
     println(paginationHelper.pagesCount())
-    println(paginationHelper.itemsPerPage(1))
-    println(paginationHelper.pageByItemId(9))
-    paginationHelper.pagePrintln(0)
-    println("__________________________________")
-    paginationHelper.pagePrintln(1)
+    println(paginationHelper.getItemsPerPage(0))
+    println(paginationHelper.getPageByElementIndex(16))
 }
 
-class PaginationHelper(array_:Array<String>,count_: Int) {
-    private val array = array_
-    private val count = count_
+class PaginationHelper(private val array:Array<String>,private val count: Int) {
+    fun length() = array.size
 
-    fun length(): Int {
-        return array.size
-    }
+    fun pagesCount() = if(array.size%count>0) array.size/count+1 else array.size/count
 
-    fun pagesCount():Int {
-        if(array.size%count>0) return array.size/count+1
-        else return array.size/count
-    }
+    fun getItemsPerPage(pageIndex: Int) =
+            when (pageIndex){
+                in 0 until pagesCount()-1 -> count
+                pagesCount()-1 -> array.size - count*(pagesCount()-1)
+                else -> 0
+            }
 
-    fun itemsPerPage(num: Int): Int {
-        if(this.pagesCount() > num+1) {
-            return count
-        } else if(this.pagesCount()== num+1) {
-            return array.size%count
-        } else {
-            return 0 //возврат 0 при запросе номера страницы превышающей количество существующих
-        }
-    }
+    fun getPageByElementIndex(elementIndex: Int) = if(elementIndex in 0..array.size) elementIndex/count else -1
 
-    fun pageByItemId(id: Int): Int {
-        if(id<array.size) {
-            if((id+1)%count>0) return (id+1)/count+1
-            else return (id+1)/count
-        } else {
-            return -1 //возврат -1 при запросе id элемента превышающего размер списка
-        }
-    }
-
-    fun pagePrintln(num: Int){
-        var i = num*count
-        while(i < num*count+count){
-            if(i<array.size) println(array[i])
-            i++
-        }
-    }
 }
